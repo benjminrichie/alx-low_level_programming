b8b065b17b34ff238737e4474956ca34b9177e03
Benjamin <benjminrichie@gmail.com>
2023-09-13 12:34:06 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,41 +5,20 @@
  * Return: Always 0
  */
 
-int main(void)
-{
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+int main() {
+    long long a = 1, b = 2, even_sum = 0;
+    long long limit = 4000000;
 
-	for (count = 0; count < 92; count++)
-	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-		fib1 = fib2;
-		fib2 = sum;
-	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 = 1;
-			half2 %= 10000000000;
-		}
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
-	}
-	printf("\n");
-	return (0);
+    while (a <= limit) {
+        if (a % 2 == 0) {
+            even_sum += a;
+        }
+        long long next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("%lld\n", even_sum);
+
+    return 0;
 }
