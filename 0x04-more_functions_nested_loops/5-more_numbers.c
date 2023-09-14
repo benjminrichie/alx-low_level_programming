5f471c5361d083e287718e6e6fa0366485bc6a2c
Benjamin <benjminrichie@gmail.com>
2023-09-14 13:27:46 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 0)
+			if (y >= 10)
 			{
 				_putchar((y / 10) + '0');
 			}
