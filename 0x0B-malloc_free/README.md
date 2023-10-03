a9bd90868b3d3b899e107b1e48cab155f249f37e
Benjamin <benjminrichie@gmail.com>
2023-10-03 11:57:03 -0700

    main.h

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+This program is a dive into malloc
+
+Program name:
+* 0x0B. C - malloc, free
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+
+
+#endif
