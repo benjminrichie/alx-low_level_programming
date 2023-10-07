9f32a84982409c750d42383573f2d5d9170577f2
Benjamin <benjminrichie@gmail.com>
2023-10-07 08:23:36 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		a = 0;
-	}
 	else
 	{
 		for (a = 0; s1[a] != '\0'; a++)
@@ -26,27 +24,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	if (s2 == NULL)
-	{
 		b = 0;
-	}
 	else
 	{
 		for (b = 0; s2[b] != '\0'; b++)
 			;
 	}
 	if (b > n)
-	{
 		b = n;
-	}
 	s = malloc(sizeof(char) * (a + b + 1));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (c = 0; c < a; c++)
 		s[c] = s1[c];
-	for (c = 0; c < b; c++)
-		s[c + 1] = s2[c];
+	for (c = 0; c < b && c < n; c++)
+		s[a + c] = s2[c];
 	s[a + b] = '\0';
 	return (s);
 }
