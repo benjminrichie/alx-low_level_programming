ee408407c6d78287530865a7873874b3fa5e407c
Benjamin <benjminrichie@gmail.com>
2023-10-19 08:01:06 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,3 +11,14 @@ void MyFunc(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
+/**
+ * main - Main func
+ * Return: 0
+ */
+
+int main(void)
+{
+	MyFunc();
+	return (0);
+}
