a7783fb5fe1ef5fbc1a8d60a393cd98e953d1ba6
Benjamin <benjminrichie@gmail.com>
2023-10-20 07:01:46 -0700

    0-print_list.c update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,26 +4,29 @@
 
 /**
  * print_list - This will print all elements
- * @h: This will be the list_t list
+ * @h: This will be the list_t(head) list
  * Return: Simply returns the number of nodes in h
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t ben_nodes = 0;
+	int ben_nodes;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	for (ben_nodes = 1; h->next != NULL; ben_nodes++)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			printf("[%u] %s\n", h->len, "(nil)");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
-		ben_nodes++;
 		h = h->next;
 	}
+	printf("[%u] %s\n", h->len, h->str);
 	return (ben_nodes);
 }
