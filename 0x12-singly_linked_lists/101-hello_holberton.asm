99e4dcfd1dcd0f84398005ee3bad6d1cbc5521af
Benjamin <benjminrichie@gmail.com>
2023-10-20 06:36:33 -0700

    101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,9 @@
-/** This is a 64-bit program in assembly that prints Hello, Holberton, followed by a new line. */
-
-global    main
-          extern    printf
+global	main
+	extern	printf
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
+	  mov edi, format
+	  xor eax, eax
+	  call printf
+	  mov	eax, 0
 	  ret
 format: db `Hello, Holberton\n`,0
-
