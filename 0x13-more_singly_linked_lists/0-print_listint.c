ad2e58cf1edca3addcbb1469e0004161861c8ddf
Benjamin <benjminrichie@gmail.com>
2023-10-24 01:56:05 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - This program is written and meant to print
+ * the elements of a listint_t
+ *
+ * @h:This is just a pointer to the very first node of the list
+ *
+ * Return: nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t myProjectNodes = 0;
+
+	for (myProjectNodes = 0; h; myProjectNodes++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (myProjectNodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+This is Benjamins README file on
+***0x13. C - More singly linked lists
