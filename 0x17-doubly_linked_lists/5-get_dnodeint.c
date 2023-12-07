02b0e9a37740d03c46cb9c4293bd3e5ba7c77a33
Benjamin <benjminrichie@gmail.com>
2023-12-07 04:33:15 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -29,8 +29,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		{
 			return (tmp);
 		}
-		size++;
-		tmp = tmp->next;
+	size++;
+	tmp = tmp->next;
 	}
 	return (NULL);
 }
