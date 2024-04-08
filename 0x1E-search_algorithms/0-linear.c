f846dffb56d0911100777d30169b9ffede4d0bce
Benjamin <benjminrichie@gmail.com>
2024-04-08 07:14:56 -0700

    All done

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Func will search for a value in
+ * an array of integers using the Linear search algorithm
+ *
+ * @array: The input array
+ * @size: THe size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
+}
